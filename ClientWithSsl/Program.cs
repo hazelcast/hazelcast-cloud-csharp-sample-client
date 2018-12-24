238e9ba188cccbbf900487521d53dc7970088a6a
Asım Arslan <arslanasim@gmail.com>
2018-12-24 12:45:12 +0300

    c# code sample with and without ssl

---
+++ ClientWithSsl/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Threading;
+using Hazelcast.Client;
+using Hazelcast.Config;
+
+namespace Client
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            var config = new ClientConfig();
+            config.GetGroupConfig()
+                .SetName("YOUR_CLUSTER_NAME")
+                .SetPassword("YOUR_CLUSTER_PASSWORD");
+            
+            config.GetNetworkConfig().GetCloudConfig()
+                .SetEnabled(true)
+                .SetDiscoveryToken("YOUR_CLUSTER_DISCOVERY_TOKEN");
+            
+            config.GetNetworkConfig().GetSSLConfig()
+                .SetEnabled(true)
+                .SetProperty(SSLConfig.CertificateFilePath, "CLIENT_PFX_CERTIFICATE_PATH")
+                .SetProperty(SSLConfig.CertificatePassword, "CLIENT_PFX_CERTIFICATE_PASSWORD");
+
+            var client = HazelcastClient.NewHazelcastClient(config);
+            var map = client.GetMap<string, string>("map");
+            map.Put("key", "value");
+            if(map.Get("key").Equals("value"))
+            {
+                Console.WriteLine("Connection Successful!");
+                Console.WriteLine("Now, `map` will be filled with random entries.");
+            }
+            else {
+                throw new Exception("Connection failed, check your configuration.");
+            }
+            var random = new Random();
+            while (true) {
+                var randomKey = random.Next(100_000);
+                map.Put("key" + randomKey, "value" + randomKey);
+                map.Get("key" + random.Next(100_000));
+                if(randomKey % 10 == 0 )
+                {
+                    Console.WriteLine("map size: {0}", map.Size());
+                }
+                Thread.Sleep(100);
+            }
+        }
+    }
+}
