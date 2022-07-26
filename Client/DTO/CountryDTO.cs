28815b96acb8c4cbf4ec26209b4e51d3ebf5397a
emreyigit <emre.yigit@hazelcast.com>
2022-07-26 15:17:57 +0300

    Examples updated.

---
+++ Client/DTO/CountryDTO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.Json.Serialization;
+
+namespace Client
+{
+    internal class CountryDTO
+    {
+        [JsonPropertyName("isoCode")]
+        public string ISOCode { get; set; }
+        [JsonPropertyName("country")]
+        public string Country { get; set; }
+    }
+}
---
+++ ClientWithSsl/DTO/CountryDTO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.Json.Serialization;
+
+namespace ClientWithSsl
+{
+    internal class CountryDTO
+    {
+        [JsonPropertyName("isoCode")]
+        public string ISOCode { get; set; }
+        [JsonPropertyName("country")]
+        public string Country { get; set; }
+    }
+}
